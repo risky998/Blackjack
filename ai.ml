5f06af13be5f5aace1372ecffd61e47a1dab70dd
Rishabh  Sarup <rs868@cornell.edu>
2020-04-24 07:39:32 -0400

    Added code for doubling strategy, will be used later

--- ai.ml
+++ ai.ml
@@ -13,6 +13,17 @@ let hit_stay_strategy c player =
   | p when p = 9 && c >= 3 && c <= 5 -> Hit
   | _ -> Hit
 
+(* This code is temporarily unusable until we get the double command working, which will be part of MS2 *)
+(* let hit_double_stay_strategy c player = 
+   let value = Player.value_hand player in 
+   match value with
+   | p when p >= 13 && c <= 6 -> Stay
+   | p when p = 12 && c >= 4 && c <= 6 -> Stay
+   | p when p = 11 && c <= 10 -> Double
+   | p when p = 10 && c <= 9 -> Double
+   | p when p = 9 && c >= 3 && c <= 5 -> Double
+   | _ -> Hit *)
+
 let probability_bust player = 
   let value = Player.value_hand player in 
   match value with
