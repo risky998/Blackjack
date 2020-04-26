35f81d6a0c47232bbb8fd13c349204fae4a478a8
samuellye98 <sl2982@cornell.edu>
2020-04-26 13:13:54 +0800

    update gitignor

--- _build/authors.mli
+++
@@ -1,7 +0,0 @@
-(**
-   Metadata about this submission.
-   Kazuya Chue (kc843), Rishabh Sarup (rs868), Samuel Lye (sl2982)
-*)
-
-(** [hours_worked] is the number of hours you worked on this assignment. *)
-val hours_worked : int
