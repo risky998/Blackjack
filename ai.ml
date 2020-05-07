590c10a99630fdfb7cfd922be335e05260abc79e
Rishabh  Sarup <rs868@cornell.edu>
2020-05-07 14:08:36 -0400

    ai module

--- ai.ml
+++ ai.ml
@@ -9,29 +9,27 @@ let dealer_strategy dealer =
   |n when n>=17 -> Stay
   | _ -> Hit
 
-let hit_stay_strategy c player = 
-  let value = Player.value_hand player in 
-  match value with
-  | p when p >= 18 -> Stay
-  | p when p >= 13 && c <= 6 -> Stay
-  | p when p = 12 && c >= 4 && c <= 6 -> Stay
-  | p when p = 11 && c <= 10 -> Hit
-  | p when p = 10 && c <= 9 -> Hit
-  | p when p = 9 && c >= 3 && c <= 5 -> Hit
-  | _ -> Hit
-
-(* This code is temporarily unusable until we get the double command working, which will be part of MS2 *)
-(* let hit_double_stay_strategy c player = 
+(* let hit_stay_strategy c player = 
    let value = Player.value_hand player in 
    match value with
+   | p when p >= 18 -> Stay
    | p when p >= 13 && c <= 6 -> Stay
    | p when p = 12 && c >= 4 && c <= 6 -> Stay
-   | p when p = 11 && c <= 10 -> Double
-   | p when p = 10 && c <= 9 -> Double
-   | p when p = 9 && c >= 3 && c <= 5 -> Double
+   | p when p = 11 && c <= 10 -> Hit
+   | p when p = 10 && c <= 9 -> Hit
+   | p when p = 9 && c >= 3 && c <= 5 -> Hit
    | _ -> Hit *)
 
-(* More code will be added here to describe the soft strategy for the AI when they have exactly two cards and the dealer's top card is being shown. This will mean that Split and Double need to be functional *)
+(* This code is temporarily unusable until we get the double command working, which will be part of MS2 *)
+let hit_double_stay_strategy c player = 
+  let value = Player.value_hand player in 
+  match value with
+  | p when p = 11 && c <= 10 -> Double
+  | p when p = 10 && c <= 9 -> Double
+  | p when p = 9 && c >= 3 && c <= 5 -> Double
+  | p when p >= 13 && c <= 6 -> Stay
+  | p when p = 12 && c >= 4 && c <= 6 -> Stay
+  | _ -> Hit
 
 let probability_bust player = 
   let value = Player.value_hand player in 
